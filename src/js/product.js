a43dc7207f8d815388b0bd3b7ed2f11e2fc23c4c
dereklp <adhoc607gmail.com>
2026-01-10 22:59:52 -0500

    Add Item to Cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,12 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const productsInLocalStorage = getLocalStorage("so-cart", product) || [];
+  productsInLocalStorage.push(product);
+  setLocalStorage("so-cart", productsInLocalStorage);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
