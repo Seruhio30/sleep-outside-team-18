c9d5a1e8a8e5ec41843a862d06897fc0880daff5
josedvega <davarela231@gmail.com>
2026-01-16 23:56:11 -0600

    David Individual W02

--- src/js/product.js
+++ src/js/product.js
@@ -1,13 +1,10 @@
-import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./productDetails.mjs";
 
 const dataSource = new ProductData("tents");
 
 const productId = getParam("product");
-
-console.log(dataSource.findProductById(productId));
-
 
 const productDet = new ProductDetails(productId, dataSource);
 productDet.init();
